0018a482407921a101106c257c3463e017ae4d0e
Irurnnen <i19v199a56n78@gmail.com>
2024-10-23 01:15:33 +0300

    Add parserExpression, validateExpression, ToPostfix

--- calc.go
+++ calc.go
@@ -1,8 +1,100 @@
 package ordinarycalc
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+const allowedSymbolsRegular = `[^0-9+\-*\/()^\s]`
+const spacesRegular = `\s`
+
+var operators = "+-*/()^"
+var priority = map[string]int{"+": 1, "-": 1, "*": 2, "/": 2, "^": 3}
+
 func Calc(expression string) (float64, error) {
-	// TODO: Remove all unnecessary characters in the expression
-	// TODO: get prioritization of the expression solution
+	// Checking validity of expression
+	if err := ValidateExpression(expression); err != nil {
+		return 0, err
+	}
+	// Delete all space in expression
+	expression = RemoveSpaces(expression)
+	// tokens, err := parseExpression(expression)
+	// if err != nil {
+	// 	return 0, err
+	// }
+	tokens := ParseExpression(expression)
+	fmt.Println(tokens)
+	fmt.Println(ToPostfix(tokens))
 	// TODO: calculate the expression
 	return 0, nil
+}
+
+func ValidateExpression(expression string) error {
+	re := regexp.MustCompile(allowedSymbolsRegular)
+	if re.MatchString(expression) {
+		return ErrExtraCharacters
+	}
+	if strings.Count(expression, "(") != strings.Count(expression, ")") {
+		return ErrUnpairedBracket
+	}
+	return nil
+}
+
+func RemoveSpaces(expression string) string {
+	re := regexp.MustCompile(spacesRegular)
+	return re.ReplaceAllString(expression, "")
+}
+
+func ParseExpression(expression string) []string {
+	var tokens []string
+	var number string
+
+	for _, character := range expression {
+		if !strings.ContainsRune(operators, character) {
+			number += string(character)
+			continue
+		}
+		if number != "" {
+			tokens = append(tokens, number)
+			number = ""
+		}
+		tokens = append(tokens, string(character))
+	}
+	return tokens
+}
+
+func ToPostfix(tokens []string) []string {
+	var stack []string
+	var output []string
+
+	for _, token := range tokens {
+		if !strings.Contains(operators, token) {
+			output = append(output, token)
+			continue
+		}
+		switch token {
+		case "(":
+			stack = append(stack, token)
+		case ")":
+			for len(stack) != 0 && stack[len(stack)-1] != "(" {
+				output = append(output, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			if len(stack) != 0 {
+				stack = stack[:len(stack)-1]
+			}
+		default:
+			for len(stack) != 0 && stack[len(stack)-1] != "(" && priority[token] <= priority[stack[len(stack)-1]] {
+				output = append(output, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			stack = append(stack, token)
+		}
+	}
+	for len(stack) != 0 {
+		output = append(output, stack[len(stack)-1])
+		stack = stack[:len(stack)-1]
+	}
+	return output
 }
--- errors.go
+++ errors.go
@@ -1,3 +1,12 @@
 package ordinarycalc
 
-// TODO: describe the errors
+import (
+	"errors"
+)
+
+// Internal errors
+var ErrRegexp = errors.New("failed to set regexp")
+
+// expression errors
+var ErrExtraCharacters = errors.New("expression have extra characters")
+var ErrUnpairedBracket = errors.New("expression have unpaired brackets")
